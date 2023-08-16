045e30665168ca82730e186edb5187dde65b972d
michaelfehr <michael_fehrgmx.de>
2023-08-16 14:12:44 +0200

    updated

--- app/src/main/java/de/androidcrypto/taplinxexample/KeyInfoProvider.java
+++ app/src/main/java/de/androidcrypto/taplinxexample/KeyInfoProvider.java
@@ -1,12 +1,4 @@
-package de.androidcrypto.taplinxexample;/*
- * *****************************************************************************************************************************
- * Copyright 2019-2020 NXP.
- * NXP Confidential. This software is owned or controlled by NXP and may only be used strictly in accordance with the applicable license terms.
- * By expressly accepting such terms or by downloading, installing, activating and/or otherwise using the software, you are agreeing that you have read, and that you agree to comply with and are bound by, such license terms.
- * If you do not agree to be bound by the applicable license terms, then you may not retain, install, activate or otherwise use the software.
- * ********************************************************************************************************************************
- *
- */
+package de.androidcrypto.taplinxexample;
 
 import android.content.Context;
 
@@ -18,75 +10,78 @@ import java.security.Key;
 /**
  * KeyInfoProvider is used to store and retrieve the keys required by the sample application.
  * Created by NXP on 7/25/2016.
+ *
+ * The TapLinx SDK supports third-party key stores. The sample app shows the implementation of the
+ * Spongy Castle library as key store. You are free to use any other key store you want.
  */
 
 public class KeyInfoProvider {
 
-    private static KeyInfoProvider mSelf = null;
     /**
-     * We shall be using Spongy Castle(Bouncy Castle for Android) to securely store and retrieve
-     * keys used in the application.
+     * We shall be using Spongy Castle(Bouncy Castle for Android) to securely store and retrieve keys used in the application.
      *
      * @see SpongyCastleKeystoreHelper
      */
-    private SpongyCastleKeystoreHelper mKeystoreHelper;
+    private SpongyCastleKeystoreHelper mKeystoreHelper = null;
+
+    private static KeyInfoProvider mSelf = null;
 
     /**
      * Private Constructor.
+     * @param context
      */
-    private KeyInfoProvider(Context context) {
+    private KeyInfoProvider(Context context){
 
-        /*
-          Necessary step to use provider of Spongy Castle.
+        /**
+         * Necessary step to use provider of Spongy Castle.
          */
         SpongyCastleKeystoreHelper.initProvider();
 
-        /*
-          Initialize the Keystore helper that helps store and retrieve keys
+        /**
+         * Initialize the Keystore helper that helps store and retrieve keys
          */
         mKeystoreHelper = new SpongyCastleKeystoreHelper(context);
     }
 
     /**
      * Returns Singleton instance of KeyInfoProvider.
-     *
+     * @param context
      * @return KeyInfoProvider
      */
-    public synchronized static KeyInfoProvider getInstance(Context context) {
-        if (mSelf == null) {
+    public synchronized static  KeyInfoProvider getInstance(Context context){
+        if(mSelf == null)
             mSelf = new KeyInfoProvider(context);
-        }
         return mSelf;
     }
 
 
     /**
      * Stores the Key to the underlying Keystore.
+     * @param alias
+     * @param keyType
+     * @param key
      */
-    public void setKey(final String alias, final SampleAppKeys.EnumKeyType keyType, final byte[] key) {
-        if (alias != null && key != null) {
+    public void setKey(final String alias, final SampleAppKeys.EnumKeyType keyType, final byte[] key){
+        if(alias != null && key != null)
             mKeystoreHelper.storeKey(key, alias, keyType);
-        }
     }
 
 
     /**
      * Retrieves the Key data from Underlying Keystore.
-     *
+     * @param alias
+     * @param keyType
      * @return IKeyData
      */
-    public IKeyData getKey(final String alias, final SampleAppKeys.EnumKeyType keyType) {
-        /*
-          MIFARE Keys are custom keys, they are not supported by SpongyCastle based keystore and
-          hence cannot be retrieved from SpongyCastle Keystore without compromising the key
-          material.
-          You can use the  method getMifareKey() to fetch Mifare Key bytes.
+    public IKeyData getKey(final String alias, final SampleAppKeys.EnumKeyType keyType){
+        /**
+         * MIFARE Keys are custom keys, they are not supported by SpongyCastle based keystore and hence cannot be retrieved from SpongyCastle Keystore without compromising the key material.
+         * You can use the  method getMifareKey() to fetch Mifare Key bytes.
          */
-        if (keyType == SampleAppKeys.EnumKeyType.EnumMifareKey) {
+        if(keyType == SampleAppKeys.EnumKeyType.EnumMifareKey)
             return null;
-        }
 
-        Key storedKey = mKeystoreHelper.getKey(alias);
+        Key storedKey =  mKeystoreHelper.getKey(alias);
         if (storedKey != null) {
             KeyData keyDataObj = new KeyData();
             keyDataObj.setKey(storedKey);
@@ -98,13 +93,11 @@ public class KeyInfoProvider {
 
     /**
      * Returns the bytes of Mifare type key.
-     *
+     * @param alias
      * @return byte[]
      */
-    public byte[] getMifareKey(final String alias) {
+    public byte[] getMifareKey(final String alias){
         return mKeystoreHelper.getMifareKey(alias);
-        //MIFARE Keys are custom keys, they are not supported by SpongyCastle based keystore and
-        // hence cannot be retrieved from SpongyCastle Keystore without compromising the key
-        // material.
+        //MIFARE Keys are custom keys, they are not supported by SpongyCastle based keystore and hence cannot be retrieved from SpongyCastle Keystore without compromising the key material.
     }
 }
